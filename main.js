f29dd848cd814df7b294f80720b24fd59fe7e388
FurkanUtkuEMRE <furkanutkuemre@hotmail.com>
2022-12-26 23:25:42 +0300

    feat: improve game logic

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// Result messages. Lose, tie and win conditions are added depending on the choices.
+let LOSE = 'N/A';
+let TIE = 'N/A';
+let WIN = 'N/A';
+let UNSUPPORTED = 'You have given an unsupported input...';
+let ERROR = 'Something went terribly wrong...';
+
+// Set the number of rounds.
+let numberOfRounds = prompt('How many rounds do you wish to play?');
+
+// Check if the input given is correct.
+if (isNaN(parseInt(numberOfRounds, 10))) {
+  console.log(UNSUPPORTED);
+}
+
 // Array of strings which the computer can pick from.
 const computerOptions = ['Rock', 'Paper', 'Scissors'];
 
@@ -11,19 +26,15 @@ function formatPlayerChoice(playerChoice) {
   return playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
 }
 
-// Set the computer choice to a variable.
-let computerChoice = getComputerChoice();
-
-let playerChoice = formatPlayerChoice('rOcK');
-
 // Results for the game.
-const LOSE = `${computerChoice} beats ${playerChoice}. Unfortunately, you lose.`;
-const TIE = `You both have ${computerChoice}. It's a tie!`;
-const WIN = `${playerChoice} beats ${computerChoice}. You win!`;
-const ERROR = `Something went terribly wrong...`;
+function setResultMessages(computerChoice, playerChoice) {
+  LOSE = `${computerChoice} beats ${playerChoice}. Unfortunately, you lose.`;
+  TIE = `You both have ${computerChoice}. It's a tie!`;
+  WIN = `${playerChoice} beats ${computerChoice}. You win!`;
+}
 
 // Round conditionals.
-function playRound() {
+function playRound(computerChoice, playerChoice) {
   if (computerChoice === playerChoice) {
     return TIE;
   } else if (computerChoice === 'Rock' && playerChoice === 'Paper') {
@@ -38,9 +49,39 @@ function playRound() {
     return WIN;
   } else if (computerChoice === 'Scissors' && playerChoice === 'Paper') {
     return LOSE;
+  } else if (playerChoice !== 'Rock' && playerChoice !== 'Paper' && playerChoice !== 'Scissors') {
+    return UNSUPPORTED;
   } else {
     return ERROR;
   }
 }
 
-console.log(playRound());
+function game() {
+  // Declare variables for the game.
+  let computerChoice;
+  let playerInput;
+  let playerChoice;
+  let roundResult;
+
+  for (let i = 0; i < numberOfRounds; i++) {
+    // Set the computer choice to a variable.
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+
+    // Get the player choice and format it.
+    playerInput = prompt('Make your choice: Rock, Paper or Scissors');
+    playerChoice = formatPlayerChoice(playerInput);
+    console.log(playerChoice);
+
+    // Set the result messages depending on the choices.
+    setResultMessages(computerChoice, playerChoice);
+
+    // Set the round result to a variable.
+    roundResult = playRound(computerChoice, playerChoice);
+
+    // Log the result to the player.
+    console.log(roundResult);
+  }
+}
+
+game();
